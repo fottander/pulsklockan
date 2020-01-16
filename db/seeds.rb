a7b3054684ab75294d338cee85bc632f157f4872
felixottander <felix.ottander@yahoo.com>
2020-01-16 19:57:42 +0100

    jjööj

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,15 @@
 Category.delete_all
+Brand.delete_all
 
 Category.create(
   name: "Pulsklockor",
   id: 199
 )
 
+Brand.create(
+  name: "Garmin",
+  id: 199
+)
+
 puts "Created #{Category.count} categories"
+puts "Created #{Brand.count} brands"
