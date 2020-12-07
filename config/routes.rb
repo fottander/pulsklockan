957a3bf1cb9a6d352937b0f83610761ee7316885
fottander <felix.ottander@yahoo.com>
2020-12-07 15:43:50 +0100

    Fixing the route errors (#34)

--- config/routes.rb
+++ config/routes.rb
@@ -14,9 +14,11 @@ Rails.application.routes.draw do
 
   resources :kategorier, controller: 'categories', as: 'categories', only: [:show, :new, :create, :edit, :update, :index, :destroy]
 
-  resources :varumärken, controller: 'brands', as: 'brands', only: [:show, :new, :create, :edit, :update, :index, :destroy]
+  resources :brands, controller: 'brands', only: [:new, :create, :edit, :update, :index, :destroy]
 
   resources :produkter, controller: 'products', as: 'products', only: [:new, :create, :edit, :update, :index, :destroy]
 
   get '/produkter/:id', to: 'products#show'
+
+  resources :brands, controller: 'brands', as: 'brands', only: [:show], path: ''
 end
