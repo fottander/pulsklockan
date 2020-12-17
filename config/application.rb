c340ad047a6d61833814ec8a7f7b10cf492681a6
felixottander <felix.ottander@yahoo.com>
2020-12-17 15:33:14 +0100

    Ljöi

--- config/application.rb
+++ config/application.rb
@@ -1,33 +1,19 @@
 require_relative 'boot'
 
-require "rails"
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "active_storage/engine"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_mailbox/engine"
-require "action_text/engine"
-require "action_view/railtie"
-require "action_cable/engine"
-require "sprockets/railtie"
+require 'rails/all'
 
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Pulsklockan
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.generators.system_tests = nil
-    config.time_zone = "Stockholm"
-    config.active_record.default_timezone = :local
-    config.generators do |generate|
-      generate.helper false
-      generate.assets false
-      generate.view_specs false
-      generate.helper_specs false
-      generate.routing_specs false
-      generate.controller_specs false
-    end
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
